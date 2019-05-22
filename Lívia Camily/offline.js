89b475cf15955b26d5dce7f143e412d5004b5122
C4mily <50411070+C4mily@users.noreply.github.com>
2019-05-22 08:29:19 -0300

    Add files via upload

---
+++ Lívia Camily/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1558524298,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/controlinho-sheet0.png",
+		"images/botaotiro-sheet0.png",
+		"images/botaotiro-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/se1-sheet0.png",
+		"images/se1-sheet1.png",
+		"images/set4-sheet0.png",
+		"images/set4-sheet1.png",
+		"images/set2-sheet0.png",
+		"images/set2-sheet1.png",
+		"images/set3-sheet0.png",
+		"images/set3-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/destruir-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
